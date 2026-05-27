87ffa7bf03b49cae7a91996a7255077fc948093e
mosharof-dev <mosharof.dev@gmail.com>
2026-05-27 22:59:35 +0600

    Add Object-Oriented programming example with Car and Student classes

---
+++ basics-python/Object-Oriented.py
@@ -0,0 +1,23 @@
+class Car:
+    pass
+
+class Car:
+    pass
+
+car1 = Car()
+
+
+class Student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def show_info(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+
+
+student1 = Student("Mosharof", 20)
+
+student1.show_info()
